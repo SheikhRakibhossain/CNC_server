ae2d927df71e948350c2866caac65667fb497561
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-22 14:22:03 +0600

    categories data added

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const  cors = require('cors')
 //middle war for cors
 app.use(cors());
 
+const categories = require('./data/categories.json')
+
 app.get('/', (req, res)=>{
     res.send('I am running on CNC news service')
 })
+
+app.get('/categories',(req, res)=>{
+    res.send(categories)
+
+})
+
 
 app.listen(port, ()=>{
     console.log(`CNC running on server ${port}`)
